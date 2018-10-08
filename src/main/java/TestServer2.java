f21a32b9c237a1f001447c082f86c685b671de05
khapersky <khapanser@gmail.com>
2018-10-08 19:16:59 +0300

    Experimental branch

--- src/main/java/TestServer2.java
+++ src/main/java/TestServer2.java
@@ -4,6 +4,9 @@ package main.java;
 
 import java.net.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class TestServer2 {
     public static void main(String[] args) throws IOException {
@@ -13,16 +16,50 @@ public class TestServer2 {
             System.exit(1);
         }
 */
-        int portNumber = 7777;//Integer.parseInt(args[0]);
-        while(true){
-            try (
-                    ServerSocket serverSocket = new ServerSocket(portNumber);
-                    Socket clientSocket = serverSocket.accept();
+        // byte[] b;
+        byte[] send = null;
+        int portNumber = 5000;//Integer.parseInt(args[0]);
+
+        while (true) {
+            Path path = Paths.get("C:\\Users\\AKhaperskiy\\OrganizerServerFiles\\test.xml");
+            byte[] data = Files.readAllBytes(path);
+            try {
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+                Socket clientSocket = serverSocket.accept();
+                ObjectOutputStream oos = new ObjectOutputStream(clientSocket.getOutputStream());
+                ObjectInputStream ois = new ObjectInputStream(clientSocket.getInputStream());
+
+                oos.writeObject(data);
+
+                try {
+                    while (ois.readObject() != null) {
+                        send = (byte[]) ois.readObject();
+                    }
+                    System.out.println("Прочитанный 10ыйй байт = "+send[10]);
+
+                    //TODO перевести файлик в XML
+
+                    // TODO очистить send.
+                    //ois.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                    /*
                     PrintWriter out =
                             new PrintWriter(clientSocket.getOutputStream(), true);
                     BufferedReader in = new BufferedReader(
-                            new InputStreamReader(clientSocket.getInputStream()));
-            ) {
+                            new InputStreamReader(clientSocket.getInputStream()));*/
+                clientSocket.close();
+
+            } catch (IOException e) {
+                System.out.println("Exception caught when trying to listen on port "
+                        + portNumber + " or listening for a connection");
+                System.out.println(e.getMessage());
+            }
+
+
+
+       /*     {
 
                 String inputLine, outputLine;
 
@@ -38,13 +75,14 @@ public class TestServer2 {
                         //break;
                         clientSocket.close();
                 }
-            }
+            }*/
 
-            catch (IOException e) {
+       /*catch (IOException e) {
                 System.out.println("Exception caught when trying to listen on port "
                         + portNumber + " or listening for a connection");
                 System.out.println(e.getMessage());
-            }
+            }*/
+
         }
     }
 }
